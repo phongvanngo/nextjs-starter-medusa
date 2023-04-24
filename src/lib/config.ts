571332d7590f1bcc1f8e00d6956060b75a55ae0b
Phong Van Ngo <1952034@gmail.com>
2023-04-24 20:42:31 +0700

    fix product out-of-stocks successfullly

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,9 @@
 import Medusa from "@medusajs/medusa-js"
 import { QueryClient } from "@tanstack/react-query"
+import { SALE_CHANNEL_IDS } from "./constants"
 
 // Defaults to standard port for Medusa server
-let MEDUSA_BACKEND_URL = "http://localhost:9000"
+let MEDUSA_BACKEND_URL = "https://api.uat.matkinh53.com"
 
 if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
   MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
@@ -18,6 +19,10 @@ const queryClient = new QueryClient({
   },
 })
 
-const medusaClient = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
+const customMedusaClient = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
+
+const medusaClient = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 });
+
+medusaClient.products.list = (query, customHeaders) => customMedusaClient.products.list({ ...query, sales_channel_id: SALE_CHANNEL_IDS }, customHeaders)
 
 export { MEDUSA_BACKEND_URL, queryClient, medusaClient }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,4 @@
 export const IS_BROWSER = typeof window !== "undefined"
+export const SALE_CHANNEL_IDS = [
+    "sc_01GYS9G5S7DJGFEME09B7PYATM",
+];
