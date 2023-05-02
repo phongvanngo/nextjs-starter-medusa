0a4c71929ef09146c57d29da49ec34745e55c233
Phong Van Ngo <1952034@gmail.com>
2023-05-02 11:00:19 +0700

    reset original config as medusa 1.9 add middleware to get default sales channel if no sales channels choose

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,5 @@
 import Medusa from "@medusajs/medusa-js"
 import { QueryClient } from "@tanstack/react-query"
-import { SALE_CHANNEL_IDS } from "./constants"
 
 // Defaults to standard port for Medusa server
 let MEDUSA_BACKEND_URL = "https://api.uat.matkinh53.com"
@@ -19,10 +18,7 @@ const queryClient = new QueryClient({
   },
 })
 
-const customMedusaClient = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
-
-const medusaClient = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 });
-
-medusaClient.products.list = (query, customHeaders) => customMedusaClient.products.list({ ...query, sales_channel_id: SALE_CHANNEL_IDS }, customHeaders)
+const medusaClient = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
 
 export { MEDUSA_BACKEND_URL, queryClient, medusaClient }
+
